132599e5b78c6525c00b859ec326ad4c6d7a4b8a
minhkog <89618619+minhkog@users.noreply.github.com>
2024-01-26 03:31:27 +0700

    Update testsc.lua

--- testsc.lua
+++ testsc.lua
@@ -4,32 +4,7 @@ local RunService = game:GetService("RunService")
 
 pcall(function()
 	local KOGcharacter = KOGLocalPlayer.Character
-	
-	
 	local afkthreshold = 1
 	local afktimethreshold = 20 
 	KOGLocalPlayer:kick("chay toi day roi nha")
-	return
-    if not KOGcharacter or not KOGcharacter:findfirstchild("humanoidrootpart") then
-		KOGLocalPlayer:kick("khong tim thay character")
-        return
-    end
-	local humanoidrootpart = KOGcharacter:waitforchild("humanoidrootpart")
-    local lastposition = humanoidrootpart.position
-    local afktime = 0
-	
-	while true do
-		local currentposition = humanoidrootpart.position
-		if (currentposition - lastposition).magnitude > afkthreshold then
-            afktime = 0 
-        else
-            afktime = afktime + deltatime
-            if afktime >= afktimethreshold then
-                warn("local character afk")
-                KOGLocalPlayer:kick("you were kicked for being afk. muahahahasdjkhshdsd")
-            end
-        end
-        lastposition = currentposition
-		wait(1)
-	end
 end)
