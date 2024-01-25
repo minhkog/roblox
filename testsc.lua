b613fd7853e0178df54cbd27ed05428618c21ab4
minhkog <89618619+minhkog@users.noreply.github.com>
2024-01-26 03:34:15 +0700

    Update testsc.lua

--- testsc.lua
+++ testsc.lua
@@ -1,6 +1,7 @@
 local KOGPlayers = game:GetService("Players")
 local KOGLocalPlayer = KOGPlayers.LocalPlayer
 local RunService = game:GetService("RunService")
-
 local KOGcharacter = KOGLocalPlayer.Character
+local afkthreshold = 1
+local afktimethreshold = 20 
 KOGLocalPlayer:kick("chay toi day roi nha")
