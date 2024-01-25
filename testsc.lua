787fcd8349de9cbb2c38c087b237b894c7027540
minhkog <89618619+minhkog@users.noreply.github.com>
2024-01-26 02:53:41 +0700

    Update testsc.lua

--- testsc.lua
+++ testsc.lua
@@ -1,26 +1,23 @@
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-local RunService = game:GetService("RunService")
-
-local afkThreshold = 1 -- Độ lệch tọa độ để xem có chuyển động hay không
-local afkTimeThreshold = 20 -- Thời gian không chuyển động để xem là AFK (300 giây = 5 phút)
-
-local function checkPlayerAFK()
-    local character = LocalPlayer.Character
+pcall(function()
+	local Players = game:GetService("Players")
+	local LocalPlayer = Players.LocalPlayer
+	local RunService = game:GetService("RunService")
+	local character = LocalPlayer.Character
+	
+	local afkThreshold = 1
+	local afkTimeThreshold = 20 
 
     if not character or not character:FindFirstChild("HumanoidRootPart") then
 		LocalPlayer:Kick("khong tim thay character")
         return
     end
-
-    local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+	local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
     local lastPosition = humanoidRootPart.Position
     local afkTime = 0
-
-    RunService.Heartbeat:Connect(function(deltaTime)
-        local currentPosition = humanoidRootPart.Position
-        if (currentPosition - lastPosition).Magnitude > afkThreshold then
-				LocalPlayer:Kick("reset nha")
+	
+	while true do
+		local currentPosition = humanoidRootPart.Position
+		if (currentPosition - lastPosition).Magnitude > afkThreshold then
             afkTime = 0 
         else
             afkTime = afkTime + deltaTime
@@ -30,7 +27,5 @@ local function checkPlayerAFK()
             end
         end
         lastPosition = currentPosition
-    end)
-end
-
-checkPlayerAFK()
+	end
+end)
