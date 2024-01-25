9bf1df914c3e7306ec3d5c2c9d6f7cab99f46d5b
minhkog <89618619+minhkog@users.noreply.github.com>
2024-01-26 05:19:16 +0700

    Update testsc.lua

--- testsc.lua
+++ testsc.lua
@@ -5,40 +5,35 @@ local KOGcharacter = KOGLocalPlayer.Character
 local afkthreshold = 1
 local afktimethreshold = 20 
 
-pcall(function()
+local success, result = pcall(function()
 
-    local success2, result2 = pcall(function()
-        if not KOGcharacter or not KOGcharacter:FindFirstChild("HumanoidRootPart") then
-        	KOGLocalPlayer:kick("khong tim thay character")
-            return
-        end
-        
-        local humanoidrootpart = KOGcharacter:WaitForChild("HumanoidRootPart")
-        local lastposition = humanoidrootpart.Position
-        local afktime = 0    
-    end)
-    
-    if not success2 then
-        game.Players.LocalPlayer:Kick(result2)
+    if not KOGcharacter or not KOGcharacter:FindFirstChild("HumanoidRootPart") then
+    	KOGLocalPlayer:kick("khong tim thay character")
+        return
     end
+    
+    local humanoidrootpart = KOGcharacter:WaitForChild("HumanoidRootPart")
+    local lastposition = humanoidrootpart.Position
+    local afktime = 0
 	
 	while true do
-		local success, result = pcall(function()
-		    local currentposition = humanoidrootpart.Position
-    		if (currentposition - lastposition).Magnitude > afkthreshold then
-                afktime = 0 
-            else
-                afktime = afktime + 1
-                if afktime >= afktimethreshold then
-                    KOGLocalPlayer:kick("Kick because AFK! kaka")
-                end
+		local currentposition = humanoidrootpart.Position
+		if (currentposition - lastposition).Magnitude > afkthreshold then
+            afktime = 0 
+        else
+            afktime = afktime + 1
+            if afktime >= afktimethreshold then
+                KOGLocalPlayer:kick("Kick because AFK! kaka")
             end
-            lastposition = currentposition
-    		wait(1)
-    		KOGLocalPlayer:kick("Kick because AFK! kaka")    
-		end)
-        if not success then
-            game.Players.LocalPlayer:Kick(result)
         end
+        lastposition = currentposition
+		wait(1)
+		KOGLocalPlayer:kick("Kick because AFK! kaka")   
 	end
 end)
+
+if not success then
+    game.Players.LocalPlayer:kick("Lỗi xảy ra")
+else
+    game.Players.LocalPlayer:kick("Không có lỗi, tiếp tục chạy chương trình")
+end
