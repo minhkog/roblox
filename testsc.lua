09c0cff07d79ed7e608255195b856ef29f4afbd2
minhkog <89618619+minhkog@users.noreply.github.com>
2024-01-26 07:42:55 +0700

    Update testsc.lua

--- testsc.lua
+++ testsc.lua
@@ -2,30 +2,57 @@ repeat wait(.25) until game:IsLoaded() and game.Players.LocalPlayer.Character
 repeat wait(1) until game:GetService('Players').LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
 
 -- game.Players.LocalPlayer.Character
-local afkthreshold = 1
-local afktimethreshold = 3
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
 
-pcall(function()
-    if not game.Players.LocalPlayer.Character or not game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
-    	game.Players.LocalPlayer:kick("khong tim thay character")
+local LocalPlayer = Players.LocalPlayer
+local afkThreshold = 1 
+local afkTimeThreshold = 300
+
+local function calculateDistance(point1, point2)
+    local dx = point2.X - point1.X
+    local dy = point2.Y - point1.Y
+    local dz = point2.Z - point1.Z
+
+    return math.sqrt(dx^2 + dy^2 + dz^2)
+end
+
+local function onPositionChanged()
+    local character = LocalPlayer.Character
+    if not character or not character:FindFirstChild("HumanoidRootPart") then
         return
     end
-    
-    local humanoidrootpart = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
-    local lastposition = humanoidrootpart.Position
-    local afktime = 0
-	
-	while true do
-		local currentposition = humanoidrootpart.Position
-		if (currentposition - lastposition).Magnitude > afkthreshold then
-            afktime = 0 
-        else
-            afktime = afktime + 1
-            if afktime >= afktimethreshold then
-                game.Players.LocalPlayer:kick("You were kicked for being AFK.")
+
+    local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+    local lastPosition = humanoidRootPart.Position
+    local afkTime = 0
+
+    -- humanoidRootPart:GetPropertyChangedSignal("Position"):Connect(function()
+        
+    -- end)
+    local currentPosition
+    while true do
+        pcall(function()
+            currentPosition = humanoidRootPart.Position
+            local distance = calculateDistance(currentPosition, lastPosition)
+            if distance > afkThreshold then
+                afkTime = 0 
+            else
+                afkTime = afkTime + 1
+                if afkTime >= afkTimeThreshold then
+                    LocalPlayer:Kick("Kich vi dam me")
+                end
             end
-        end
-        lastposition = currentposition
-		wait(1)
-	end
+            lastPosition = currentPosition
+        end)
+        wait(.5)
+    end
+    
+end
+
+spawn(function()
+    local success, result = pcall(onPositionChanged)
+    if not success then
+        LocalPlayer:Kick("Error when connecting Position changed: " .. tostring(result))
+    end    
 end)
