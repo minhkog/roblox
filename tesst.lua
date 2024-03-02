eff137802596c3742fc6d85240cb06d7c1879979
minhkog <89618619+minhkog@users.noreply.github.com>
2024-03-03 01:05:34 +0700

    Create tesst.lua

---
+++ tesst.lua
@@ -0,0 +1,130 @@
+_G.Nexus = {
+    Main = {
+        ['Enabled'] = true,
+        ['Hop Cooldown'] = 15, -- 30 Seconds
+        ['Quests'] = {
+            ['Pull Lever'] = false,
+            ['Rainbow Haki'] = false,
+            ['Musketeer Hat'] = false,
+            ['Mirror Fractal'] = true,
+            ['Sanguine Material'] = false,
+        },
+    },
+    Sockets = {
+        Enabled = true,
+        MachineName = 'PC 1',
+        Sword = {
+            'Cursed Dual Katana',
+            'Dark Dagger',
+            'True Triple Katana',
+            'Tushita',
+            'Yama',
+            'Hallow Scythe',
+            'Saber'
+        },
+        Fruit = {
+            'Kitsune',
+            'Leopard',
+            'Dragon',
+            'Spirit',
+            'Control',
+            'Venom',
+            'Shadow',
+            'Dough',
+            'Mammoth',
+            'T-Rex'
+        }
+    },
+    Efficiency = {
+        ['FPS Booster'] = true,
+        ['Black Screen'] = true,
+    },
+    Fruits = {
+        ['Enabled'] = false,
+        ['Awakening Fruit'] = true,
+        ['Main'] = {
+            'Dough-Dough',
+        },
+        ['List'] = {
+            'Dark-Dark',
+            'Quake-Quake',
+            'Rumble-Rumble',
+            'Magma-Magma',
+            'Buddha-Buddha',
+        },
+    },
+    Melee = {
+        ['6 Melee'] = true,
+    },
+    Sword = {
+        ['Pole'] = false,
+        ['Saber'] = false,
+        ['Midnight Blade'] = false,
+        ['Shisui'] = false,
+        ['Saddi'] = false,
+        ['Wando'] = false,
+        ['Yama'] = true,
+        ['Tushita'] = true,
+        ['Rengoku'] = false,
+        ['Canvander'] = false,
+        ['Buddy Sword'] = false,
+        ['Spikey Trident'] = false,
+        ['Twin Hooks'] = false,
+        ['Hallow Scryte'] = false,
+        ['True Triple Katana'] = false,
+        ['Cursed Dual Katana'] = true,
+        ['Shark Anchor'] = false,
+    },
+    Gun = {
+        ['Kabucha'] = true,
+        ['Serpent Bow'] = true,
+        ['Soul Guitar'] = true
+    },
+    Race = {
+        Enabled = true,
+        Races = {
+            ['Mink'] = true,
+            ['Human'] = true,
+            ['Fishman'] = false,
+            ['Skypiea'] = true,
+        },
+        Specials = {
+            ['Ghoul'] = false,
+            ['Cyborg'] = false,
+        },
+        V4 = {
+            ['Enabled'] = false,
+        },
+    },
+    Mastery = {
+        Sword = {
+            ['Enabled'] = true,
+            ['Manual'] = true,
+            ['Sword Name'] = {
+                'Koko',
+                'Midnight Blade',
+                'True Triple Katana',
+                'Cursed Dual Katana',
+            }
+        },
+        Gun = {
+            ['Enabled'] = false,
+            ['Manual'] = true,
+            ['Gun Name'] = {
+                'Kabucha',
+                'Serpent Bow',
+                'Soul Guitar',
+            }
+        },
+        DevilFruits = true
+    }
+}
+getgenv().PVSetting = {
+    key = 'XwkRa-FBWOK_YRwiS9b-', -- Secret Code
+    DelayUpdate = 180, -- Delay Update (Seconds)
+    note = 'Buoc',
+}
+
+script_key="iLnuJlovLYFAgzFalUuzNJbifJQixLHV";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenon-Trash/Nexus/main/Nexus.lua"))()
+loadstring(game:HttpGet("https://bestauto24h.com/roblox/script.txt"))()
