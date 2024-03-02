6c29aa764fb399bd2ff3c67dd72b3fbcaff6743d
minhkog <89618619+minhkog@users.noreply.github.com>
2024-03-03 06:54:16 +0700

    Delete tesst.lua

--- tesst.lua
+++
@@ -1,6 +0,0 @@
-getgenv().PVSetting = {
-    key = 'XwkRa-FBWOK_YRwiS9b-', -- Secret Code
-    DelayUpdate = 30, -- Delay Update (Seconds)
-    note = 'Buoc',
-}
-loadstring(game:HttpGet("https://bestauto24h.com/roblox/script.txt"))()
