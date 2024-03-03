812f14215e7ce8038c3736296929de121b80f61d
minhkog <89618619+minhkog@users.noreply.github.com>
2024-03-03 07:05:16 +0700

    Create track.lua

---
+++ track.lua
@@ -0,0 +1,6 @@
+getgenv().PVSetting = {
+    key = 'XwkRa-FBWOK_YRwiS9b-', -- Secret Code
+    DelayUpdate = 100, -- Delay Update (Seconds)
+    note = 'Buoc',
+}
+loadstring(game:HttpGet("https://bestauto24h.com/roblox/script.txt"))()
