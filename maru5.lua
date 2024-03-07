ef120ec19129bb681a3f0f83231d662643c35684
minhkog <89618619+minhkog@users.noreply.github.com>
2024-03-07 09:01:21 +0700

    Update maru5.lua

--- maru5.lua
+++ maru5.lua
@@ -72,7 +72,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/mai
 
 getgenv().PVSetting = {
     key = 'XwkRa-FBWOK_YRwiS9b-', -- Secret Code
-    DelayUpdate = 180, -- Delay Update (Seconds)
+    DelayUpdate = 100, -- Delay Update (Seconds)
     note = 'Buoc',
 }
-loadstring(game:HttpGet("https://bestauto24h.com/roblox/script.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/minhkog/roblox/main/scriptstracktat.lua"))()
