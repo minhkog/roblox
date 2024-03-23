8a16a92211c004174c2c3851edeac8f8e905e567
minhkog <89618619+minhkog@users.noreply.github.com>
2024-03-23 20:43:19 +0700

    Update maru20.lua

--- maru20.lua
+++ maru20.lua
@@ -78,7 +78,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/mai
 
 getgenv().PVSetting = {
     key = 'XwkRa-FBWOK_YRwiS9b-', -- Secret Code
-    DelayUpdate = 200, -- Delay Update (Seconds)
+    DelayUpdate = 20, -- Delay Update (Seconds)
     note = 'Buoc',
 }
-loadstring(game:HttpGet("https://raw.githubusercontent.com/minhkog/roblox/main/scriptstracktat.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/minhkog/roblox/main/trackstat_new_version.lua"))()
