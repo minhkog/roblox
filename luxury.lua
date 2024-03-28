344a7f07a1190fd7e4e9138c2a138621fb123941
minhkog <89618619+minhkog@users.noreply.github.com>
2024-03-28 18:58:24 +0700

    Create luxury.lua

---
+++ luxury.lua
@@ -0,0 +1,8 @@
+
+
+_G.KaitanMode = true
+_G.Key = "MAXZT-JONWZ-7EVZK"
+_G.Key = "LuxuryV2_7x55s9451h8yyfkniqo0"
+
+_G.DiscordId = "599152046523940874"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NightsTimeZ/RoyryX/main/mainpack.lua"))();
