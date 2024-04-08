5e928b31c76d6e49c2aa96cb0e216fabb9729b79
minhkog <89618619+minhkog@users.noreply.github.com>
2024-04-09 05:09:44 +0700

    Update banana.lua

--- banana.lua
+++ banana.lua
@@ -1,4 +1,25 @@
-repeat task.wait() until game:IsLoaded()
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 getgenv().Key = "23e1705744810ebb"
-getgenv().OldBanana = false
-loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
+getgenv().SettingFarm ={
+    ["GodHuman"] = true,
+    ["Priority Get Melee Sea 3"] = false,
+    ["Auto Race V2-V3"] = false,
+    ["Auto Saber"] = true,
+    ["Auto Pole"] = true,
+    ["Cursed Dual Katana"] = true,
+    ["SoulGuitar"] = false,
+    ["Teleport To Fruit"] = true,
+    ["Hop Fruit 1M Quest Third Sea"] = false,
+    ["White Screen"] = false,
+    ["Buy Haki"] = {
+        ["Enhancement"] = false,
+        ["Skyjump"] = false,
+        ["Flash Step"] = false,
+        ["Observation"] = false,
+    },
+
+    ["Blox Fruit Sniper"] = {},
+    ["Lock Fruit"] = {"Dragon-Dragon","Kitsune-Kitsune","Leopard-Leopard"},
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/KaitunTrash.lua"))()
