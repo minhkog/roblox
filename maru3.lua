a6a9bc1545172b49c2913bef66fe059d7abca19b
minhkog <89618619+minhkog@users.noreply.github.com>
2024-04-21 07:53:34 +0700

    Update maru3.lua

--- maru3.lua
+++ maru3.lua
@@ -65,11 +65,69 @@ _G.GunSettings = { -- ปืนที่จะทำ
     ['SoulGuitar'] = true
 }
 
+spawn(function()
+    -- Tạo một ScreenGui
+    local screenGui = Instance.new("ScreenGui")
+    screenGui.Name = "ColorTransition"
+    screenGui.Parent = game.Players.LocalPlayer.PlayerGui
+
+    -- Tính toán kích thước và vị trí của Frame
+    local left = 115
+    local top = 71
+    local width = 417
+    local height = 35
+
+    local position = UDim2.new(0, left, 0, top)
+    local size = UDim2.new(0, width, 0, height)
+
+    -- Tạo một Frame trong ScreenGui
+    local frame = Instance.new("Frame")
+    frame.Position = position
+    frame.Size = size
+    frame.AnchorPoint = Vector2.new(0, 0)
+    frame.BackgroundColor3 = Color3.new(1, 1, 1)
+    frame.Parent = screenGui
+
+    -- Tạo một Tween với TweenService
+    local tweenService = game:GetService("TweenService")
+    local tweenInfo = TweenInfo.new(2, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut)
+
+    -- Mảng các màu sắc chuyển động
+    local colors = {
+        Color3.new(1, 0, 0), -- Đỏ
+        Color3.new(0, 1, 0), -- Xanh lá cây
+        Color3.new(0, 0, 1)  -- Xanh dương
+    }
+
+    local colorIndex = 1 -- Chỉ số màu sắc hiện tại
+
+    -- Hàm để chạy tween animation
+    local function runTween()
+        local currentColor = colors[colorIndex]
+        local nextColor = colors[colorIndex + 1] or colors[1] -- Trở lại màu đầu tiên sau khi đã chạy qua tất cả các màu
+
+        local tween = tweenService:Create(frame, tweenInfo, {BackgroundColor3 = nextColor})
+        tween:Play()
+        tween.Completed:Connect(function()
+            colorIndex = colorIndex + 1
+            if colorIndex > #colors then
+                colorIndex = 1
+            end
+            runTween() -- Gọi lại hàm để tiếp tục chạy tween animation
+        end)
+    end
+
+    -- Chạy tween animation ban đầu
+    runTween()
+end)
+
+
 getgenv().Key = "MARU-0WSG-5GN7N-8BLP-W228W-OPUT"
 getgenv().id = "523962819365044236"
 getgenv().Script_Mode = "Kaitun_Script"
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/minhkog/roblox/main/animation.lua"))()
+
 --getgenv().PVSetting = {
 --    key = 'XwkRa-FBWOK_YRwiS9b-', -- Secret Code
 --    DelayUpdate = 20, -- Delay Update (Seconds)
